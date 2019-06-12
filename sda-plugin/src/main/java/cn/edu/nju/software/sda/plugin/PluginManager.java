342ff1784502e46016771774071bb039b2e90d8e
jachly <jachly@qq.com>
2019-06-12 16:32:33 +0800

    使用serviceloader实现插件

--- sda-plugin/src/main/java/cn/edu/nju/software/sda/plugin/PluginManager.java
+++ sda-plugin/src/main/java/cn/edu/nju/software/sda/plugin/PluginManager.java
@@ -1,8 +1,13 @@
 package cn.edu.nju.software.sda.plugin;
 
+import cn.edu.nju.software.sda.core.utils.WorkspaceUtil;
 import cn.edu.nju.software.sda.plugin.utils.PackageUtil;
 import org.apache.commons.lang3.ClassUtils;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.*;
 
 public class PluginManager {
@@ -33,6 +38,35 @@ public class PluginManager {
     public void reload(){
 //        registerSysPlugin(SysPlugin.class);
         register(new SysPlugin());
+        ClassLoader serviceCL = null;
+        try {
+            File dir = new File(WorkspaceUtil.absolutePath("/plugins"));
+            if(!dir.exists()){
+                dir.mkdirs();
+            }else{
+                if(!dir.isDirectory()){
+                    dir.delete();
+                    dir.mkdirs();
+                }
+            }
+            List<URL> urls = new ArrayList<>();
+            for (File jar:
+                    dir.listFiles()) {
+                if(jar.getName().endsWith(".jar")){
+                    urls.add(jar.toURI().toURL());
+                }
+            }
+            serviceCL = new URLClassLoader(
+                    urls.toArray(new URL[urls.size()]),
+                    Plugin.class.getClassLoader());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        ServiceLoader<Plugin> loader = ServiceLoader.load(Plugin.class, serviceCL);
+        for (Plugin plugin : loader) {
+            register(plugin);
+        }
         System.out.println("reload plugin end");
     }
 
