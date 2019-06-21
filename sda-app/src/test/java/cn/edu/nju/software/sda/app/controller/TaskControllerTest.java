84be2d9c1f8e44cc4537991f7507024cc8c6d648
jachly <jachly@qq.com>
2019-06-21 13:33:01 +0800

    Add Task Test

--- sda-app/src/test/java/cn/edu/nju/software/sda/app/controller/TaskControllerTest.java
+++ sda-app/src/test/java/cn/edu/nju/software/sda/app/controller/TaskControllerTest.java
@@ -1,6 +1,7 @@
 package cn.edu.nju.software.sda.app.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -51,8 +53,51 @@ public class TaskControllerTest {
         MockMultipartFile firstFile = new MockMultipartFile("file",
                 "dddsample-2.0-SNAPSHOT.jar", "text/plain", inputStream);
 
-        mvc.perform(MockMvcRequestBuilders.multipart("/api/upload")
+        String result = mvc.perform(MockMvcRequestBuilders.multipart("/api/upload")
                 .file(firstFile))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        JSONObject file = JSONObject.parseObject(result);
+        String filePath = file.getString("path");
+
+        result = mvc.perform(MockMvcRequestBuilders.post("/api/task/do")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content("{\"appId\":\""+appId+"\",\"type\":\"InfoCollection\",\"functionName\"" +
+                        ":\"sys_StaticJavaInfoCollection\",\"inputDataDto\":" +
+                        "{\"infoDatas\":{},\"formDatas\":{\"Package\":[\"se.citerus.dddsample\"]," +
+                        "\"Jar/War\":[\""+filePath+"\"]}}}"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+        JSONObject task = JSONObject.parseObject(result);
+        String taskId = task.getString("id");
+        assert (StringUtils.isNotBlank(taskId));
+
+        String status = task.getString("status");
+        assert (StringUtils.isNotBlank(status));
+        // /api/task/1906219345RW2AK4
+        Long time = System.currentTimeMillis();
+        while("Doing".equals(status)) {
+            assert (System.currentTimeMillis() - time < 3000);
+
+            Thread.sleep(200);
+            result = mvc.perform(MockMvcRequestBuilders
+                    .get("/api/task/" + taskId))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andReturn()
+                    .getResponse()
+                    .getContentAsString();
+            task = JSONObject.parseObject(result);
+
+            status = task.getString("status");
+            assert (StringUtils.isNotBlank(status));
+            if("Complete".equals(status)){
+                break;
+            }
+            assert (!"Error".equals(status));
+        }
     }
 }
